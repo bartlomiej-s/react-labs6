c0817f010e809b7f4892d3bbae591f847774e55e
Bartek <bartek77@live.com>
2019-12-01 21:51:35 +0100

    Lab8 Task 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+
 import PageEmployee from './PageEmployee'
 import PageEmployeesList from './PageEmployeesList'
 
@@ -12,9 +20,16 @@ class App extends React.Component {
 
   render() {
     const AppVar = (
-     <div className="App">
-       <PageEmployeesList />
-     </div>
+     <Router>
+       <Switch>
+         <Route exact path="/">
+           <PageEmployeesList></PageEmployeesList>
+         </Route>
+         <Route path="/new">
+           <PageEmployee></PageEmployee>
+         </Route>
+       </Switch>
+     </Router>
     )
 
     return AppVar
